0a5f56ff8a499e3f075e8f7492b60428de2cc1b8
AdrianCreate <adrianalexandru1307@gmail.com>
2026-04-02 02:08:09 +0300

    feat: implement activity editing and deletion commands; add AutoMapper configuration

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,5 @@
 using Application.Activities;
+using Application.Activities.Commands;
 using Application.Activities.Queries;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
@@ -23,5 +24,21 @@ public class ActivitiesController : BaseApiController
     public async Task<ActionResult<string>> CreateActivity(Activity activity)
     {
         return await Mediator.Send(new CreateActivity.Command { Activity = activity });
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> EditActivity(Activity activity)
+    {
+        await Mediator.Send(new EditActivity.Command { Activity = activity });
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteActivity(string id)
+    {
+        await Mediator.Send(new DeleteActivity.Command { Id = id });
+
+        return Ok();
     }
 }
