09a88cd10f9d2d7962aac60a809802348e66c814
AdrianCreate <adrianalexandru1307@gmail.com>
2026-04-17 16:42:56 +0300

    feat: implement routing and refactor components for improved navigation; add HomePage and MenuItemLink

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,85 +1,103 @@
 import { Box, Button, Paper, TextField, Typography } from '@mui/material'
 import type { FormEvent } from 'react'
 import { useActivities } from '../../../lib/hooks/useActivities'
+import { useNavigate, useParams } from 'react-router'
 
-type Props = {
-  activity?: Activity
-  closeForm: () => void
-}
+export default function ActivityForm() {
+    const { id } = useParams()
+    const { updateActivity, createActivity, activity, isLoadingActivity } =
+        useActivities(id)
+    const navigate = useNavigate()
 
-export default function ActivityForm({ activity, closeForm }: Props) {
-  const { updateActivity, createActivity } = useActivities()
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
+        const data: { [key: string]: FormDataEntryValue } = {}
+        formData.forEach((value, key) => {
+            data[key] = value
+        })
 
-    const data: { [key: string]: FormDataEntryValue } = {}
-    formData.forEach((value, key) => {
-      data[key] = value
-    })
-
-    if (activity) {
-      data.id = activity.id
-      await updateActivity.mutateAsync(data as unknown as Activity)
-      closeForm()
-    } else {
-      await createActivity.mutateAsync(data as unknown as Activity)
-      closeForm()
+        if (activity) {
+            data.id = activity.id
+            await updateActivity.mutateAsync(data as unknown as Activity)
+            navigate(`/activities/${activity.id}`)
+        } else {
+            createActivity.mutate(data as unknown as Activity, {
+                onSuccess: (id) => {
+                    navigate(`/activities/${id}`)
+                },
+            })
+        }
     }
-  }
 
-  return (
-    <Paper sx={{ borderRadius: 3, padding: 3 }}>
-      <Typography variant="h5" gutterBottom color="primary">
-        Create activity
-      </Typography>
-      <Box
-        component="form"
-        onSubmit={handleSubmit}
-        display="flex"
-        flexDirection="column"
-        gap={3}
-      >
-        <TextField name="title" label="Title" defaultValue={activity?.title} />
-        <TextField
-          name="description"
-          label="Description"
-          multiline
-          rows={3}
-          defaultValue={activity?.description}
-        />
-        <TextField
-          name="category"
-          label="Category"
-          defaultValue={activity?.category}
-        />
-        <TextField
-          name="date"
-          label="Date"
-          type="date"
-          defaultValue={
-            activity?.date
-              ? new Date(activity.date).toISOString().split('T')[0]
-              : new Date().toISOString().split('T')[0]
-          }
-        />
-        <TextField name="city" label="City" defaultValue={activity?.city} />
-        <TextField name="venue" label="Venue" defaultValue={activity?.venue} />
-        <Box display="flex" justifyContent="end" gap={3}>
-          <Button type="button" onClick={closeForm} color="inherit">
-            Cancel
-          </Button>
-          <Button
-            type="submit"
-            color="success"
-            variant="contained"
-            loading={updateActivity.isPending}
-          >
-            Submit
-          </Button>
-        </Box>
-      </Box>
-    </Paper>
-  )
+    if (isLoadingActivity) return <Typography>Loading activity...</Typography>
+
+    return (
+        <Paper sx={{ borderRadius: 3, padding: 3 }}>
+            <Typography variant="h5" gutterBottom color="primary">
+                {activity ? 'Edit activity' : 'Create activity'}
+            </Typography>
+            <Box
+                component="form"
+                onSubmit={handleSubmit}
+                display="flex"
+                flexDirection="column"
+                gap={3}
+            >
+                <TextField
+                    name="title"
+                    label="Title"
+                    defaultValue={activity?.title}
+                />
+                <TextField
+                    name="description"
+                    label="Description"
+                    multiline
+                    rows={3}
+                    defaultValue={activity?.description}
+                />
+                <TextField
+                    name="category"
+                    label="Category"
+                    defaultValue={activity?.category}
+                />
+                <TextField
+                    name="date"
+                    label="Date"
+                    type="date"
+                    defaultValue={
+                        activity?.date
+                            ? new Date(activity.date)
+                                  .toISOString()
+                                  .split('T')[0]
+                            : new Date().toISOString().split('T')[0]
+                    }
+                />
+                <TextField
+                    name="city"
+                    label="City"
+                    defaultValue={activity?.city}
+                />
+                <TextField
+                    name="venue"
+                    label="Venue"
+                    defaultValue={activity?.venue}
+                />
+                <Box display="flex" justifyContent="end" gap={3}>
+                    <Button type="button" color="inherit">
+                        Cancel
+                    </Button>
+                    <Button
+                        type="submit"
+                        color="success"
+                        variant="contained"
+                        loading={updateActivity.isPending}
+                    >
+                        Submit
+                    </Button>
+                </Box>
+            </Box>
+        </Paper>
+    )
 }
